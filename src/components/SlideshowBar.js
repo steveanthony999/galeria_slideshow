1fad1e5c53218f9234459dc00363d2c43b9597d8
Steven Woodward <steveanthony999@gmail.com>
2021-10-12 22:02:41 -0700

    instantiated slideshow bar component

---
+++ src/components/SlideshowBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SlideshowBar = () => {
+  return (
+    <div>
+      <h1>Slideshow Bar</h1>
+    </div>
+  );
+};
+
+export default SlideshowBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
+import SlideshowBar from '../components/SlideshowBar';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <SlideshowBar />
     </div>
   );
 };
