11be7da7639caa8bc260355b49c16260e04090ab
Steven Woodward <steveanthony999@gmail.com>
2021-10-13 21:57:26 -0700

    set up slideshow bar

---
+++ src/components/slideshowBar/SlideshowBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './slideshowBar.css';
+
+const SlideshowBar = () => {
+  return (
+    <header>
+      <h1 className='heading-two'>galleria.</h1>
+      <p className='link-one dark-grey'>START SLIDESHOW</p>
+    </header>
+  );
+};
+
+export default SlideshowBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import SlideshowBar from '../components/SlideshowBar';
+import SlideshowBar from '../components/slideshowBar/SlideshowBar';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <SlideshowBar />
+      <hr />
     </div>
   );
 };
