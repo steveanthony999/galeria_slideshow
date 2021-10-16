82897042837d4898c6ff08f52dde4bb6934231bf
Steven Woodward <steveanthony999@gmail.com>
2021-10-16 10:10:39 -0700

    got images to display

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import SlideshowBar from '../components/slideshowBar/SlideshowBar';
 import React from 'react';
+import SlideshowBar from '../components/slideshowBar/SlideshowBar';
+
+import data from '../data';
 
 const Home = () => {
   return (
     <div>
       <SlideshowBar />
       <hr />
+      {data.map((i) => (
+        <div key={i.id}>
+          <img src={i.images.thumbnail} alt={i.name} />
+        </div>
+      ))}
     </div>
   );
 };
