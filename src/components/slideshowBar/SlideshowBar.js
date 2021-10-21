9d0826626150e7621ee95e9d39d3bd2234b29caf
Steven Woodward <steveanthony999@gmail.com>
2021-10-20 21:23:31 -0700

    added class name and hover effect to slideshow start link

--- src/components/slideshowBar/SlideshowBar.js
+++ src/components/slideshowBar/SlideshowBar.js
@@ -5,7 +5,7 @@ const SlideshowBar = () => {
   return (
     <header>
       <h1 className='heading-two'>galleria.</h1>
-      <p className='link-one dark-grey'>START SLIDESHOW</p>
+      <p className='SlideshowBar-start link-one dark-grey'>START SLIDESHOW</p>
     </header>
   );
 };
