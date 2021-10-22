8b94aed231a3ec09e916e1461237da3f84a34c55
Steven Woodward <steveanthony999@gmail.com>
2021-10-21 21:04:21 -0700

    setup slideshow toggler

--- src/components/slideshowBar/SlideshowBar.js
+++ src/components/slideshowBar/SlideshowBar.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './slideshowBar.css';
 
 const SlideshowBar = () => {
+  const [slides, setSlides] = useState(false);
+
+  const toggleSlideshow = () => {
+    setSlides((slides) => !slides);
+  };
+
   return (
     <header className='SlideshowBar'>
       <Link className='heading-two black' to='/'>
         galleria.
       </Link>
-      <Link className='SlideshowBar-start link-one dark-grey' to='/1'>
-        START SLIDESHOW
-      </Link>
+      {/* <Link className='SlideshowBar-start link-one dark-grey' to='/1'> */}
+      <div
+        className='SlideshowBar-toggle link-one dark-grey'
+        onClick={toggleSlideshow}>
+        {!slides ? 'START SLIDESHOW' : 'STOP SLIDESHOW'}
+      </div>
+      {/* </Link> */}
     </header>
   );
 };
