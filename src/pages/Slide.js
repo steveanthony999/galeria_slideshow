95bd2657448b07fc779e0cc595cca0d2d56b7565
Steven Woodward <steveanthony999@gmail.com>
2021-10-21 20:30:44 -0700

    set up Slide component

--- src/pages/Slide.js
+++ src/pages/Slide.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router';
 
+import data from '../data';
+
 const Slide = () => {
   const { id } = useParams();
 
   return (
     <div className='Slide'>
       <h1>Slideshow {id}</h1>
+      <p>{data[id - 1].year}</p>
+      <img src={data[id - 1].images.thumbnail} alt='' />
     </div>
   );
 };
