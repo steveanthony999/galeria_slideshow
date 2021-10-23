56fd9ed60b979fd61fe1aa0dd5d1bf202c2373f2
Steven Woodward <steveanthony999@gmail.com>
2021-10-23 10:10:40 -0700

    added back and next media buttons

--- src/components/bottomBar/BottomBar.js
+++ src/components/bottomBar/BottomBar.js
@@ -1,9 +1,14 @@
+import BackButton from '../../assets/shared/icon-back-button.svg';
+import NextButton from '../../assets/shared/icon-next-button.svg';
+
 import './BottomBar.css';
 
 const BottomBar = () => {
   return (
     <div className='BottomBar'>
       <h1>Bottom Bar</h1>
+      <img src={BackButton} alt='back-button' />
+      <img src={NextButton} alt='next-button' />
     </div>
   );
 };
