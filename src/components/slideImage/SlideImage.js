a76a8a1eb54fda2084716bd11114ece1e5f8cd63
Steven Woodward <steveanthony999@gmail.com>
2021-10-29 22:48:34 -0700

    added SlideImage view link

--- src/components/slideImage/SlideImage.js
+++ src/components/slideImage/SlideImage.js
@@ -1,9 +1,14 @@
+import ViewIcon from '../../assets/shared/icon-view-image.svg';
 import './SlideImage.css';
 
 const SlideImage = ({ art, alt }) => {
   return (
     <div className='SlideImage'>
       <img src={art} alt={alt} />
+      <div className='SlideImage-view-link'>
+        <img src={ViewIcon} alt='icon' />
+        <p className='link-two white'>VIEW IMAGE</p>
+      </div>
     </div>
   );
 };
